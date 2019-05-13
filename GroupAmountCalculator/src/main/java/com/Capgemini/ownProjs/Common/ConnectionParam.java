b6bfa0664caa40ad57ac7df4b55c7e5eec76ca58
Madhuryamadhu <madhuryanmadhu@gmail.com>
2019-05-13 09:14:45 +0530

    First Commit

---
+++ GroupAmountCalculator/src/main/java/com/Capgemini/ownProjs/Common/ConnectionParam.java
@@ -0,0 +1,13 @@
+package com.Capgemini.ownProjs.Common;
+
+public class ConnectionParam {
+
+	public static final String DRIVER="com.mysql.cj.jdbc.Driver";                             //"com.mysql.jdbc.Driver";
+	public static final String URL="jdbc:mysql://localhost:3306/groupamountcalculator?useSSL=false";
+	public static final String USERNAME="root";
+	public static final String PASSWORD="root";
+	
+	//Pdf File Upload Path
+	public static final String PDF_UPLOAD_PATH="F:\\ProjectDetails\\GroupProject\\GroupAmountCalculator\\PdfFolder";
+	
+}
