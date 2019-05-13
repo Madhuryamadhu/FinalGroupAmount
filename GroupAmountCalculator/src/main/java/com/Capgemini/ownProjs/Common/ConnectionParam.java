04139a7a6dba5304854e99bf337ae9fcae00a842
pradeep.mascarenhas <paddum19@gmail.com>
2019-05-14 01:21:04 +0530

    Final DB updates

--- GroupAmountCalculator/src/main/java/com/Capgemini/ownProjs/Common/ConnectionParam.java
+++ GroupAmountCalculator/src/main/java/com/Capgemini/ownProjs/Common/ConnectionParam.java
@@ -4,9 +4,9 @@ public class ConnectionParam {
 
 	public static final String DRIVER="com.mysql.cj.jdbc.Driver";                             //"com.mysql.jdbc.Driver";
 	//public static final String URL="jdbc:mysql://localhost:3306/groupamountcalculator?useSSL=false";
-	public static final String URL="jdbc:mysql://127.0.0.1:3306/root?useSSL=false";
-	public static final String USERNAME="usernNJ";
-	public static final String PASSWORD="jAepoXFN";
+	public static final String URL="jdbc:mysql://mysql:3306/groupamountdb?useSSL=false";
+	public static final String USERNAME="userIV4";
+	public static final String PASSWORD="Gj2dTAedVcoOWgIi";
 	
 	//Pdf File Upload Path
 	//public static final String PDF_UPLOAD_PATH="F:\\ProjectDetails\\GroupProject\\GroupAmountCalculator\\PdfFolder";
